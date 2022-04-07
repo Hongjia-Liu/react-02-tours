242890ea1ab6a0dd6f4e0a8bf046e0b34ddec96a
Hongjia Liu <hongjia_liu@outlook.com>
2022-04-07 13:00:04 +0100

    Fetch tours data

--- src/App.js
+++ src/App.js
@@ -6,6 +6,22 @@ function App() {
 	const [loading, setloading] = useState(true);
 	const [tours, setTours] = useState([]);
 
+	const fetchTours = async () => {
+		try {
+			const response = await fetch(url);
+			const tours = await response.json();
+			setloading(false);
+			setTours(tours);
+		} catch (error) {
+			setloading(false);
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		fetchTours();
+	}, []);
+
 	return loading ? (
 		<main>
 			<Loading />
