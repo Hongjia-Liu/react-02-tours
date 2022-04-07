21a9902f700de20b21cb900f2b0a35fd129f5bac
Hongjia Liu <hongjia_liu@outlook.com>
2022-04-07 12:58:55 +0100

    Setup conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tour";
 const url = "https://course-api.com/react-tours-project";
 function App() {
-	return <h2>Tours Project Setup</h2>;
+	const [loading, setloading] = useState(true);
+	const [tours, setTours] = useState([]);
+
+	return loading ? (
+		<main>
+			<Loading />
+		</main>
+	) : (
+		<main>
+			<Tours tours={tours} />
+		</main>
+	);
 }
 
 export default App;
